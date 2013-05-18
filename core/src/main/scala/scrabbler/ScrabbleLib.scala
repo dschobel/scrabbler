fbc6bcb76e8803ad4c847d626c784600595c5da2
Daniel Schobel <dschobel@gmail.com>
2013-05-18 15:09:51 -0400

    implement scoring function

--- core/src/main/scala/scrabbler/ScrabbleLib.scala
+++ core/src/main/scala/scrabbler/ScrabbleLib.scala
@@ -8,5 +8,15 @@ import ExecutionContext.Implicits.global
 
 
 object SearchUtils {
-  def score(word: String): Int = 0
+
+  lazy val CharScores = Map[Char,Int]( 'A' -> 1, 'E' -> 1, 'I' -> 1, 'L' -> 1,
+                                       'N' -> 1, 'O' -> 1, 'R' -> 1, 'S' -> 1,
+                                       'T' -> 1, 'U' -> 1, 'D' -> 2, 'G' -> 2,
+                                       'B' -> 3, 'C' -> 3, 'M' -> 3, 'P' -> 3,
+                                       'F' -> 4, 'H' -> 4, 'V' -> 4, 'W' -> 4,
+                                       'Y' -> 4, 'K' -> 5, 'J' -> 8, 'X' -> 8,
+                                       'Q' -> 10, 'Z' -> 10)
+
+
+  def score(str: String) = str.foldLeft(0){(acc,c) => acc + CharScores(c.toUpper)}
 }
--- core/src/test/scala/scrabbler/ScrabbleLibTests.scala
+++ core/src/test/scala/scrabbler/ScrabbleLibTests.scala
@@ -5,9 +5,44 @@ import org.scalatest.FunSpec
 import org.scalatest.BeforeAndAfter
 
 class ScrabbleLibTests extends FunSpec with BeforeAndAfter {
-  describe("thing") {
-    it("is true by default") {
-      assert(true)
+  describe("score function") {
+    it("scores a zero character word as zero") {
+      assert(SearchUtils.score("") === 0)
+    }
+
+    it("returns the correct score for every character of the alphabet"){
+      assert(SearchUtils.score("a") === 1)
+      assert(SearchUtils.score("b") === 3)
+      assert(SearchUtils.score("c") === 3)
+      assert(SearchUtils.score("d") === 2)
+      assert(SearchUtils.score("e") === 1)
+      assert(SearchUtils.score("f") === 4)
+      assert(SearchUtils.score("g") === 2)
+      assert(SearchUtils.score("h") === 4)
+      assert(SearchUtils.score("i") === 1)
+      assert(SearchUtils.score("j") === 8)
+      assert(SearchUtils.score("k") === 5)
+      assert(SearchUtils.score("l") === 1)
+      assert(SearchUtils.score("m") === 3)
+      assert(SearchUtils.score("n") === 1)
+      assert(SearchUtils.score("o") === 1)
+      assert(SearchUtils.score("p") === 3)
+      assert(SearchUtils.score("q") === 10)
+      assert(SearchUtils.score("r") === 1)
+      assert(SearchUtils.score("s") === 1)
+      assert(SearchUtils.score("t") === 1)
+      assert(SearchUtils.score("u") === 1)
+      assert(SearchUtils.score("v") === 4)
+      assert(SearchUtils.score("w") === 4)
+      assert(SearchUtils.score("x") === 8)
+      assert(SearchUtils.score("y") === 4)
+      assert(SearchUtils.score("z") === 10)
+    }
+
+    it("returns the correct score for words with multiple characters"){
+      assert(SearchUtils.score("cat") === 5)
+      assert(SearchUtils.score("aaa") === 3)
+
     }
   }
 }
